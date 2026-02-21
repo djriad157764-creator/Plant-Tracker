40028bdd85e66ae2be6aa3ae1a00c6523f2b468d
djriad157764-creator <djriad157764@gmail.com>
2026-02-21 15:14:40 +0600

    fix issue

--- scripts.js
+++ scripts.js
@@ -31,6 +31,8 @@ function calculateCount() {
 
 calculateCount();
 
+
+
 // btn style and section toggle
 
 // function toggleStyle(id) {
@@ -84,55 +86,64 @@ calculateCount();
 //   });
 // }
 
-function toggleStyle(id) {
-  currentStatus = id;
+// function toggleStyle(id) {
+//   currentStatus = id;
 
-  // reset all btn style
+//   // reset all btn style
 
-  [allFilterBtn, thrivingFilterBtn, strugglingFilterBtn].forEach((btn) => {
-    btn.className =
-      "px-8 py-3 rounded-2xl backdrop-blur-lg transition-all duration-300 border border-white/10 font-bold";
-    if (btn.id === id) {
-      btn.classList.add(
-        "bg-blue-600/40",
-        "text-white",
-        "border-blue-400/50",
-        "shadow-[0_0_20px_rgba(59,130,246,0.3)]",
-        "scale-105",
-      );
-    } else {
-      btn.classList.add("bg-white/5", "text-white/40", "hover:bg-white/10");
-    }
-  });
+//   [allFilterBtn, thrivingFilterBtn, strugglingFilterBtn].forEach((btn) => {
+//     btn.className =
+//       "px-8 py-3 rounded-2xl backdrop-blur-lg transition-all duration-300 border border-white/10 font-bold";
+//     if (btn.id === id) {
+//       btn.classList.add(
+//         "bg-blue-600/40",
+//         "text-white",
+//         "border-blue-400/50",
+//         "shadow-[0_0_20px_rgba(59,130,246,0.3)]",
+//         "scale-105",
+//       );
+//     } else {
+//       btn.classList.add("bg-white/5", "text-white/40", "hover:bg-white/10");
+//     }
+//   });
 
-  const allCards = document.querySelectorAll(".card");
-  allCards.forEach((card) => {
-    const statusText = card.querySelector(".status").innerText;
-    let shouldShow =
-      id === "all-filter-btn" ||
-      (id === "thriving-filter-btn" && statusText === "Thrive") ||
-      (id === "struggling-filter-btn" && statusText === "Struggle");
+//   const allCards = document.querySelectorAll(".card");
+//   allCards.forEach((card) => {
+//     const statusText = card.querySelector(".status").innerText;
+//     let shouldShow =
+//       id === "all-filter-btn" ||
+//       (id === "thriving-filter-btn" && statusText === "Thrive") ||
+//       (id === "struggling-filter-btn" && statusText === "Struggle");
 
-    if (shouldShow) {
-      card.style.display = "block";
-      setTimeout(() => {
-        card.style.opacity = "1";
-        card.style.transform = "translateY(0) scale(1)";
-      }, 10);
-    } else {
-      card.style.opacity = "0";
-      card.style.transform = "translateY(20px) scale(0.95)";
-      setTimeout(() => {
-        card.style.display = "none";
-      }, 400);
-    }
-  });
-}
+//     if (shouldShow) {
+//       card.style.display = "block";
+//       setTimeout(() => {
+//         card.style.opacity = "1";
+//         card.style.transform = "translateY(0) scale(1)";
+//       }, 10);
+//     } else {
+//       card.style.opacity = "0";
+//       card.style.transform = "translateY(20px) scale(0.95)";
+//       setTimeout(() => {
+//         card.style.display = "none";
+//       }, 400);
+//     }
+//   });
+// }
 
 // working btn
 
 mainContainer.addEventListener("click", function (event) {
   const target = event.target;
+
+  if (
+    !target.classList.contains("thriving-btn") &&
+    !target.classList.contains("struggling-btn") &&
+    !target.classList.contains("btn-delete")
+  ) {
+    return;
+  }
+
   const card = target.closest(".card");
 
   if (!card) return;
@@ -142,10 +153,10 @@ mainContainer.addEventListener("click", function (event) {
   // if click thrive btn
   if (target.classList.contains("thriving-btn")) {
     statusEi.innerText = "Thrive";
-     card.classList.remove("struggle-active");
-     card.classList.add("thrive-active");
-     statusEi.className =
-       "status px-3 py-1 rounded-full bg-emerald-500/20 border border-emerald-500/40 text-[10px] font-bold uppercase tracking-[2px] text-emerald-400";
+    card.classList.remove("struggle-active");
+    card.classList.add("thrive-active");
+    statusEi.className =
+      "status px-3 py-1 rounded-full bg-emerald-500/20 border border-emerald-500/40 text-[10px] font-bold uppercase tracking-[2px] text-emerald-400";
     // thrivingList update
 
     if (!thrivingList.includes(plantName)) {
@@ -158,9 +169,6 @@ mainContainer.addEventListener("click", function (event) {
   } // if click struggling btn
   else if (target.classList.contains("struggling-btn")) {
     statusEi.innerText = "Struggle";
-    
-   
-
 
     card.classList.remove("thrive-active");
     card.classList.add("struggle-active");
